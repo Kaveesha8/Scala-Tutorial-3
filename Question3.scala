5891d871b684e5d5344f40addd53aa14578ec7dd
Kaveesha Muthukuda <84612267+Kaveesha8@users.noreply.github.com>
2022-07-18 22:21:20 +0530

    Update Question3.scala

--- Question3.scala
+++ Question3.scala
@@ -1,7 +1,7 @@
 import scala.math._  //import math package
 object question3 extends App(){
     def main(radius:Double):Double={      //Volume of a sphere
-        return  4/3*Pi*radius*radius*radius
+        return  4*Pi*radius*radius*radius/3
     }
     println(main(5))
 
